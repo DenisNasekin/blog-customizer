ece78d95dd318a1bcda0e22bdf4fdf714c790d3d
Denis Nasekin <nasekindo@yandex.ru>
2024-03-20 20:10:45 +0500

    сделал наполнение формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,8 +1,20 @@
 import { ArrowButton } from 'components/arrow-button';
+import { Text } from 'components/text';
+import { Select } from 'components/select';
+import { RadioGroup } from 'components/radio-group';
+import { Separator } from 'components/separator';
 import { Button } from 'components/button';
+
 import { useState, useRef, useEffect } from 'react';
 import styles from './ArticleParamsForm.module.scss';
-import { Select } from '../select';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
 export const ArticleParamsForm = () => {
 	const [form, setForm] = useState<boolean>(false);
@@ -17,7 +29,42 @@ export const ArticleParamsForm = () => {
 			<ArrowButton state={form} openForm={() => setForm(!form)} />
 			<aside className={styles.container} ref={formRef}>
 				<form className={styles.form}>
-					<Select selected={null} options={[]} />
+					<Text
+						as='h2'
+						size={31}
+						weight={800}
+						family={'open-sans'}
+						uppercase
+						dynamicLite>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={defaultArticleState.fontFamilyOption}
+					/>
+					<RadioGroup
+						name='font size'
+						title='размер шрифта'
+						options={fontSizeOptions}
+						selected={defaultArticleState.fontSizeOption}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={defaultArticleState.fontColor}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={defaultArticleState.backgroundColor}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={defaultArticleState.contentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
