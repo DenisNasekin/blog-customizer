6d6c944ecb23e930e9d45f8c72cedc1729d50926
Denis Nasekin <nasekindo@yandex.ru>
2024-03-20 19:20:16 +0500

    сделал открытие формы

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,6 +1,6 @@
 import arrow from 'src/images/arrow.svg';
 import styles from './ArrowButton.module.scss';
-import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
@@ -10,18 +10,29 @@ type ArrowButtonProps = {
 	openForm: OnClick;
 };
 export const ArrowButton = (props: ArrowButtonProps) => {
+	const buttonRef = useRef<HTMLDivElement>(null);
+	const arrowRef = useRef<HTMLImageElement>(null);
+
+	useEffect(() => {
+		if (buttonRef.current)
+			buttonRef.current.classList.toggle(styles.container_open);
+		if (arrowRef.current) arrowRef.current.classList.toggle(styles.arrow_open);
+	}, [props.state]);
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={clsx(props.state ? styles.container_open : styles.container)}
-			onClick={props.openForm}>
+			className={styles.container}
+			onClick={props.openForm}
+			ref={buttonRef}>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={clsx(props.state ? styles.arrow_open : styles.arrow)}
+				className={styles.arrow}
+				ref={arrowRef}
 			/>
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,15 +1,23 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from '../select';
 
 export const ArticleParamsForm = () => {
-	const [openForm, setOpenFrom] = useState<boolean>(false);
+	const [form, setForm] = useState<boolean>(false);
+	const formRef = useRef<HTMLFormElement>(null);
+
+	useEffect(() => {
+		if (formRef.current)
+			formRef.current.classList.toggle(styles.container_open);
+	}, [form]);
 	return (
 		<>
-			<ArrowButton state={openForm} openForm={() => setOpenFrom} />
-			<aside className={styles.container}>
+			<ArrowButton state={form} openForm={() => setForm(!form)} />
+			<aside className={styles.container} ref={formRef}>
 				<form className={styles.form}>
+					<Select selected={null} options={[]} />
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
